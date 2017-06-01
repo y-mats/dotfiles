4e105ae681bb6e57a5967a8b995957be82a3d605
MATSUBARA, Yuki <yuki.matsubara@ntt-at.co.jp>
2017-06-01 11:24:31 +0900

    wget オプション修正, install コマンドの権限修正

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@ if [ ! -f ${HOME}/.git-prompt.sh ]
 then
   wget \
     'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh' \
-    --output-file=${HOME}/.git-prompt.sh
+    -O ${HOME}/.git-prompt.sh
 fi
 if [ ! -f ${HOME}/.git-completion.bash ]
 then
   wget \
     'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash' \
-    --output-file=${HOME}/.git-completion.bash
+    -O ${HOME}/.git-completion.bash
 fi
 
 cd $(dirname $0)
 current_dir=$(pwd)
-install ${current_dir}/bashrc ${HOME}/.bashrc
-install ${current_dir}/vimrc  ${HOME}/.vimrc
+install -m 664 ${current_dir}/bashrc ${HOME}/.bashrc
+install -m 664 ${current_dir}/vimrc  ${HOME}/.vimrc
